9bcaad14f86f8e13c5e8530dcab2f5db02e167b2
mohan <mohan.vive@gmail.com>
2018-11-09 22:13:21 +0530

    Add minor reformating changes

--- tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/seal/TupleTypeSealInbuiltFunctionTest.java
+++ tests/ballerina-unit-test/src/test/java/org/ballerinalang/test/expressions/seal/TupleTypeSealInbuiltFunctionTest.java
@@ -20,7 +20,6 @@ package org.ballerinalang.test.expressions.seal;
 import org.ballerinalang.launcher.util.BCompileUtil;
 import org.ballerinalang.launcher.util.BRunUtil;
 import org.ballerinalang.launcher.util.CompileResult;
-import org.ballerinalang.model.types.BAnyType;
 import org.ballerinalang.model.types.BAnydataType;
 import org.ballerinalang.model.types.BRecordType;
 import org.ballerinalang.model.types.BStringType;
